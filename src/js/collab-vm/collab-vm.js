e629041c991dd06087a05e17331267ddd7c1e03d
yellows111 <ice_ice@email.com>
2021-06-20 19:49:09 +0100

    slight mishap

--- src/js/collab-vm/collab-vm.js
+++ src/js/collab-vm/collab-vm.js
@@ -1182,7 +1182,7 @@ window.multicollab = function(ip) {
 			// If the image is empty, then this is a VM that uses
 			// computernewb screenshots. Otherwise, we should use the base64
 			// payload the server sends.
-			if (thisnode.ip.split(":")[0]==window.location.hostname||thisnode.ip.split(":")[0]=="computernewb.com"||thisnode.ip.split(":")[0]=="cvm.amogus.uk") {
+			if (thisnode.ip.split(":")[0]==window.location.hostname||thisnode.ip.split(":")[0]=="computernewb.com") {
 			isoffical = '<span data-tooltip="Official VM" data-delay="5000" data-position="top left" data-variation="tiny">&nbsp;<i class="star icon"></i></span>';}
 			else { isoffical = ""; }
 			if (thisnode.requiresPassword == 1) {
